251cafc5790e7bb4442a1c0e48f7b385cb9b96a7
unst4b13 <unst4b13@pm.me>
2023-09-29 15:21:57 -0500

    adding yummy and removing sudo make

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ $# -ne 1 ]; then
+	# TODO - verify this is URL
 	echo "Usage: ./$0 <kernel tarball link>"
 	exit -1
 fi
 
-URL=$1
 KERN_DIR="kernel-dir"
-PROC_COUNT=$(lscpu | awk '/^CPU\(s\)/ {print $2}')
+PROC_COUNT=$(($(nproc) * 2))
 
 echo 'set -o vi' >> ~/.bashrc
 echo 'hi comment ctermfg=red' >> ~/.vimrc
@@ -19,22 +19,39 @@ if which apt; then
 		libelf-dev bison qemu-system-x86 gdb dwarves
 fi;
 
+if which yum; then
+	sudo yum -y update
+	sudo yum -y install autoconf automake binutils bison \
+   	flex gcc gdb glibc-devel libtool make pkgconf \
+   	pkgconf-m4 pkgconf-pkg-config redhat-rpm-config 
+	sudo yum -y install epel-release
+	#recall this working on 9 and failing on 8
+	if sudo yum config-manager --set-enabled powertools; then
+		sudo yum -y install dwarves
+	else
+		echo "Manually install PowerTools repo"
+		RELEASE=$(cat /etc/*release)
+		echo "$RELEASE"
+		exit 2
+	fi
+fi
+
 wget -O ${KERN_DIR}.tar.xz $1
 mkdir ${KERN_DIR}
 tar -xf ${KERN_DIR}.tar.xz -C ${KERN_DIR}/ --strip-components=1
 
 cp /boot/config-$(uname -r) ${KERN_DIR}/.config
 
-sudo make -C ${KERN_DIR} kvm_guest.config
-sudo sed -i '/CONFIG_RANDOMIZE_BASE/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYRING/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_SYSTEM_REVOCATION_LIST/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_SYSTEM_REVOCATION_KEYS/s/^/#/' ${KERN_DIR}/.config
-sudo sed -i '/CONFIG_FRAME_WARN=1024/c CONFIG_FRAME_WARN=2048' ${KERN_DIR}/.config
-sudo echo 'CONFIG_KASAN=y' >> ${KERN_DIR}/.config
-sudo echo 'CONFIG_KASAN_INLINE=y' >> ${KERN_DIR}/.config
+make -C ${KERN_DIR} kvm_guest.config
+sed -i '/CONFIG_RANDOMIZE_BASE/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_SYSTEM_TRUSTED_KEYRING/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_SYSTEM_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_SYSTEM_REVOCATION_LIST/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_SYSTEM_REVOCATION_KEYS/s/^/#/' ${KERN_DIR}/.config
+sed -i '/CONFIG_FRAME_WARN=1024/c CONFIG_FRAME_WARN=2048' ${KERN_DIR}/.config
+echo 'CONFIG_KASAN=y' >> ${KERN_DIR}/.config
+echo 'CONFIG_KASAN_INLINE=y' >> ${KERN_DIR}/.config
 
 make -C ${KERN_DIR} savedefconfig
 make -C ${KERN_DIR} -j${PROC_COUNT}
