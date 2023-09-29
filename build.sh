0dbf6863f4f95c28b94ae6f8aecdd6ebbfa50c54
unst4b13 <unst4b13@pm.me>
2023-09-29 13:34:46 -0500

    increase frame size to 2048

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then
 fi
 
 URL=$1
-KERN_VER="kernel-dir"
+KERN_DIR="kernel-dir"
 PROC_COUNT=$(lscpu | awk '/^CPU\(s\)/ {print $2}')
 
 echo 'set -o vi' >> ~/.bashrc
@@ -19,22 +19,23 @@ if which apt; then
 		libelf-dev bison qemu-system-x86 gdb dwarves
 fi;
 
-wget -O ${KERN_VER}.tar.xz $1
-mkdir ${KERN_VER}
-tar -xf ${KERN_VER}.tar.xz -C ${KERN_VER}/ --strip-components=1
+wget -O ${KERN_DIR}.tar.xz $1
+mkdir ${KERN_DIR}
+tar -xf ${KERN_DIR}.tar.xz -C ${KERN_DIR}/ --strip-components=1
 
-cp /boot/config-$(uname -r) ${KERN_VER}/.config
+cp /boot/config-$(uname -r) ${KERN_DIR}/.config
 
-sudo make -C ${KERN_VER} kvm_guest.config
-sudo sed -i '/CONFIG_RANDOMIZE_BASE/s/^/#/' ${KERN_VER}/.config
-sudo sed -i '/CONFIG_TRUSTED_KEYS/s/^/#/' ${KERN_VER}/.config
-sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYRING/s/^/#/' ${KERN_VER}/.config
-sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYS/s/^/#/' ${KERN_VER}/.config
-sudo sed -i '/CONFIG_SYSTEM_REVOCATION_LIST/s/^/#/' ${KERN_VER}/.config
-sudo sed -i '/CONFIG_SYSTEM_REVOCATION_KEYS/s/^/#/' ${KERN_VER}/.config
-sudo echo 'CONFIG_KASAN=y' >> ${KERN_VER}/.config
-sudo echo 'CONFIG_KASAN_INLINE=y' >> ${KERN_VER}/.config
+sudo make -C ${KERN_DIR} kvm_guest.config
+sudo sed -i '/CONFIG_RANDOMIZE_BASE/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYRING/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_SYSTEM_TRUSTED_KEYS/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_SYSTEM_REVOCATION_LIST/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_SYSTEM_REVOCATION_KEYS/s/^/#/' ${KERN_DIR}/.config
+sudo sed -i '/CONFIG_FRAME_WARN=1024/c CONFIG_FRAME_WARN=2048' ${KERN_DIR}/.config
+sudo echo 'CONFIG_KASAN=y' >> ${KERN_DIR}/.config
+sudo echo 'CONFIG_KASAN_INLINE=y' >> ${KERN_DIR}/.config
 
-make -C ${KERN_VER} savedefconfig
-make -C ${KERN_VER} -j${PROC_COUNT}
+make -C ${KERN_DIR} savedefconfig
+make -C ${KERN_DIR} -j${PROC_COUNT}
 
