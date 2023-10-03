b1a37aa2a5cfba00d3d02f18c7157178d5df8485
unst4b13 <unst4b13@pm.me>
2023-10-03 16:35:20 -0500

    adding elf work to project

---
+++ net/accept.c
@@ -0,0 +1,28 @@
+#include <linux/module.h>
+#include <linux/socket.h>
+#include <linux/ftrace.h>
+#include <linux/kallsyms.h>
+
+void callback_func(unsigned long, unsigned long, struct ftrace_ops *, struct ftrace_regs *);
+
+struct ftrace_ops ops = {
+	.func = callback_func,
+	.flags = FTRACE_OPS_FL_SAVE_REGS_IF_SUPPORTED
+};
+
+void callback_func(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct ftrace_regs *fregs) {
+	printk(KERN_INFO "Calling callback function!!!\n");
+	return;
+}
+
+int init_module(void) {
+	ftrace_set_filter(&ops, "accept", strlen("accept"), 0);
+	register_ftrace_function(&ops);
+	return 0;	
+}
+
+void cleanup_module(void) {
+	unregister_ftrace_function(&ops);
+}
+
+MODULE_LICENSE("GPL");
