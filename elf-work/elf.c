4dea7c3194ff115f57434b2091b8b7f66815c016
unst4b13 <unst4b13@pm.me>
2023-10-04 18:53:04 -0500

    cleaned up and set todos

--- elf-work/elf.c
+++ elf-work/elf.c
@@ -12,7 +12,6 @@ int main(int argc, char **argv) {
 	void *addr = NULL;
 	struct stat s;
 	
-
 	Elf64_Ehdr *ehdr;
 	Elf64_Phdr *phdr;
 	Elf64_Shdr *shdr;
@@ -27,25 +26,24 @@ int main(int argc, char **argv) {
 	addr = mmap(NULL, s.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if(!addr) 
 		return -1;
-
+	
+	//cast everything
 	ehdr = (Elf64_Ehdr *)addr;
 	phdr = (Elf64_Phdr *)(addr + ehdr->e_phoff);
 	shdr = (Elf64_Shdr *)(addr + ehdr->e_shoff);
-	
-	printf("Entry is at 0x%lx\n", ehdr->e_entry);
-	printf("ehdr is %p\n", ehdr);
-	printf("phdr is %p\n", phdr);
-	printf("shdr is %p\n", shdr);
 	orig_entry = ehdr->e_entry;
-	
+
+	//program headers	
 	for(int i = 0; i < ehdr->e_phnum; i++) {
 		if(phdr[i].p_type == PT_NOTE) {
 			phdr[i].p_type = PT_LOAD;	
 			phdr[i].p_paddr = (Elf64_Addr)0x31540;
 			phdr[i].p_vaddr = (Elf64_Addr)0x31540;
+			//TODO - add offset, alignment, exec flags
 		}
 	}	
 
+	//section headers
 	for(int i = 0; i < ehdr->e_shnum; i++) {
 		if(shdr[i].sh_type == SHT_NOTE) {
 			shdr[i].sh_type = SHT_PROGBITS;
@@ -53,6 +51,7 @@ int main(int argc, char **argv) {
 			shdr[i].sh_size = 0xabc; 
 			shdr[i].sh_flags = SHF_ALLOC | SHF_EXECINSTR;
 			shdr[i].sh_addralign = 0;
+			//TODO - add offset
 			break;
 		}	
 	}
